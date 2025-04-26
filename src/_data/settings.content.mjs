6d4f5fbf37c2d787b5ed04cf63f3a2c24843e0c4
Erwin Goossen <erwin@navelpluisje.nl>
2025-04-26 20:29:33 +0200

    Updated settings

--- src/_data/settings.content.mjs
+++ src/_data/settings.content.mjs
@@ -16,12 +16,15 @@ export default {
 </ul>
 <p>These should get you started with the FaderPort.Beside these there are a couple of settings to make your life easier:</p>
 <ul>
-    <li><b>Disable Plugin Control:</b> If you use another controller for controlling the plugins, you can select this. 
+    <li><b>Disable Plugin Control*:</b> If you use another controller for controlling the plugins, you can select this. 
         The plugins mode is still active and you are still able to open the plugin window. 
         From that point, the ReaSonus FaderPort will not do anything anymore for the plugins</li>
-    <li><b>Swap Shift buttons:</b> This swaps the left and right Shift button.</li>
+    <li><b>Swap Shift buttons*:</b> This swaps the left and right Shift button.</li>
+    <li><b>Mute/Solo momentary:</b> When set, it will make the Solo and Mute buttons momentary.
+    The timeout for being momentary is 500 milli seconds. In short: If you press the solo button longer then 500ms
+    and release it, it will instant unsolo again</li>
 </ul>
-<blockquote>These settings have no effect on the FaderPort V2</blockquote>
+<blockquote>Settings marked with a \'*\' have no effect on the FaderPort V2</blockquote>
 <p>The Documentation button should open your default browser and open this website</p>
         `,
     },
