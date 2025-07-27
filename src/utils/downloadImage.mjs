61e4bf836fc13be157a094a414724d2f0235d711
Erwin Goossen <erwin@navelpluisje.nl>
2025-07-27 21:02:56 +0200

    Connected with Strapi

---
+++ src/utils/downloadImage.mjs
@@ -0,0 +1,39 @@
+import http from "http";
+import fs from "fs";
+import path from "path";
+import "dotenv/config";
+
+function getFileName(path) {
+  const parts = path.split("/");
+  return parts[parts.length - 1];
+}
+
+export const downloadImage = (imagePath) => {
+  const imageLocation = path.join(process.cwd(), "src", "public", "cms_images");
+  const imageUrl = `${process.env.STRAPI_URL}${imagePath}`;
+  const imageName = `${imageLocation}/${getFileName(imageUrl)}`;
+
+  if (fs.existsSync(imageName)) {
+    return;
+  }
+
+  const file = fs.createWriteStream(imageName);
+
+  if (!fs.existsSync(imageLocation)) {
+    fs.mkdirSync(imageLocation, { recursive: true });
+  }
+
+  http
+    .get(imageUrl, (response) => {
+      response.pipe(file);
+
+      file.on("finish", () => {
+        file.close();
+        console.log(`Image downloaded as ${imageName}`);
+      });
+    })
+    .on("error", (err) => {
+      fs.unlink(imageName);
+      console.error(`Error downloading image: ${err.message}`);
+    });
+};
