37a8ccd87b47e4d646f5b49052b1e90a92224dca
navelpluisje <navelpluisje@users.noreply.github.com>
2025-10-24 21:58:35 +0000

    deploy: c85a5e08175e059ca82d0136bc0134a7b4a738ad

--- javascript/YM-0xG3Yeq.js
+++ javascript/YM-0xG3Yeq.js
@@ -21,6 +21,43 @@
     menuItem.addEventListener("click", handleAnchorClick);
   }
 })();
+(function imagePopout() {
+  const images = document.querySelectorAll("img.popout");
+
+  const popoutImage = (event) => {
+    const removeEventListeners = () => {
+      const element = document.getElementById("image-popout");
+      element.removeEventListener("click", handleImageClick);
+      document.body.removeEventListener("keydown", handleKeyClick);
+    };
+
+    const handleImageClick = () => {
+      removeEventListeners();
+
+      document.getElementById("image-container").innerHTML = "";
+    };
+
+    const handleKeyClick = (event) => {
+      if (event.key === "Escape") {
+        removeEventListeners();
+
+        document.getElementById("image-container").innerHTML = "";
+      }
+    };
+
+    const html = `<section id="image-popout" class="image-popout"><img src="${event.target.src}"<section>`;
+
+    document.getElementById("image-container").innerHTML = html;
+
+    const element = document.getElementById("image-popout");
+    element.addEventListener("click", handleImageClick);
+    document.body.addEventListener("keydown", handleKeyClick);
+  };
+
+  for (const image of Array.from(images)) {
+    image.addEventListener("click", popoutImage);
+  }
+})();
 (function tabs() {
   const tabs = document.querySelectorAll(".tab-bar button");
   const tabContents = document.querySelectorAll(".tab-content .tab");
